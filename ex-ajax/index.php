d1488643d5eb9ed72272e8f968ee4d19d0c2d393
Aurora Grippaudo <aurora268@hotmail.it>
2021-12-21 16:41:07 +0100

    setting up

---
+++ ex-ajax/index.php
@@ -0,0 +1,3 @@
+<!-- Stampare a schermo i dischi musicali (file allegato) in due modi diversi:
+- Solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. Teniamo i dati dei dischi in un file separato.
+- Utilizzando Html, JS, Vue e Axios (CDN), PHP: Al caricamento della pagina axios chiederà attraverso una chiamata API i dischi a file PHP e li stamperà attraverso Vue. -->
---
+++ ex-php/index.php
@@ -0,0 +1,15 @@
+<!-- Stampare a schermo i dischi musicali (file allegato) in due modi diversi:
+- Solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. Teniamo i dati dei dischi in un file separato.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
